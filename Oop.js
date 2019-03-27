cbd0aa5ff03c3f17e0787e56732943304a4caff8
RaedShihab <raedshihab92@gmail.com>
2019-03-27 10:24:07 +0200

    solution

--- Oop.js
+++ Oop.js
@@ -7,24 +7,50 @@ function randInt(n) {
 }
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
+  var obj={};
+  
+  obj.upper=upperbound;
+  obj.rand=randInt(obj.upper)
+  obj.counter=0;
+
+
+  reurn obj;
+    
   }
-}
+  
+    guessMyNumber:function(n){
+
+
+    counter++;
+      if (n > upper) {
+        return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+      } else if (n === rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    },
+    giveUp:function(){
+      return rand;
+    },
+    numOfGuesses:function(){
+      return counter;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
